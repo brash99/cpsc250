5019d42a189e07f9851444a70c73acedb80f9000
brash99 <brash@jlab.org>
2023-04-25 11:55:21 -0400

    Updates

---
+++ Zylabs/Chapter1/1.12/main.py
@@ -0,0 +1,4 @@
+user_num = int(input())
+user_num_squared = user_num * user_num   # Bug here; fix it when instructed
+   
+print(user_num_squared)                # Output formatting issue here; fix it when instructed
