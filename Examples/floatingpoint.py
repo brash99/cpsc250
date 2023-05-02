75b2250f272569eaa1ca1459c57e2b85f2750c1d
brash99 <brash@jlab.org>
2023-05-02 19:05:05 -0400

    updates

---
+++ Examples/floatingpoint.py
@@ -0,0 +1,24 @@
+import math
+
+a = 0.1
+b = 0.1
+c = 0.1
+
+# Here is a dangerous algorithm
+print("Algorithm 1:")
+if a+b+c == 0.3:
+    print("Sanity")
+else:
+    print("Insanity")
+
+# Here is a better algorithm!
+print("Algorithm 2:")
+epsilon = 1.0E-10
+expectedValue = 0.3
+
+difference = (a+b+c) - expectedValue
+
+if math.fabs(difference) < epsilon:
+    print("Sanity")
+else:
+    print("Insanity")
