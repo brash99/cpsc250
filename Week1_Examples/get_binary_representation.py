b72a833d766dd464d76ef9e99da3821aaa535e40
brash99 <brash@jlab.org>
2023-05-06 17:45:40 -0400

    updates

--- Week1_Examples/get_binary_representation.py
+++ Week1_Examples/get_binary_representation.py
@@ -3,20 +3,21 @@ def convert_int_to_binary(num):
 
     while num > 0:
         digit = num % 2
-        #print(num, digit)
+        # print(num, digit)
         res = str(digit) + res
         num = int(num / 2)
 
     return res
 
-def convert_fraction_to_binary(num,p):
+
+def convert_fraction_to_binary(num, p):
     count = 0
     res = ""
 
     while count < p:
         comparison = 2 ** (-1 - count)
-        #print (num,comparison,res)
-        if (num >= comparison):
+        # print (num,comparison,res)
+        if num >= comparison:
             res = res + "1"
             num = num - comparison
         else:
@@ -27,76 +28,82 @@ def convert_fraction_to_binary(num,p):
     return res
 
 
-n = input("Enter your floating point value : \n")
+def get_fp_binary_representation(n):
+    # Take user input for the number of
+    # decimal places user want result as
+    # p = int(input("Enter the number of decimal places of the result : \n"))
+    p = 48
 
-# Take user input for the number of
-# decimal places user want result as
-#p = int(input("Enter the number of decimal places of the result : \n"))
-p = 48
+    # Step 1:  split the number into two parts - both strings
+    front, back = str(n).split('.')
 
-# Step 1:  split the number into two parts - both strings
-front,back = str(n).split('.')
+    # Step 2:  convert the part in front of the decimal to binary
+    if int(front) < 0:
+        sign = "-"
+        i_front = -int(front)
+    else:
+        sign = ""
+        i_front = int(front)
 
-# Step 2:  convert the part in front of the decimal to binary
-if (int(front)<0):
-    sign = "-"
-    ifront = -int(front)
-else:
-    sign = ""
-    ifront = int(front)
+    if i_front == 0:
+        front_bin = "0"
+    else:
+        front_bin = convert_int_to_binary(i_front)
 
-if (ifront == 0):
-    front_bin = "0"
-else:
-    front_bin = convert_int_to_binary(ifront)
+    # Step 3:  convert the part after the decimal to binary
+    divisor = 10**len(back)
+    back_bin = convert_fraction_to_binary(float(back)/divisor, p)
 
-# Step 3:  convert the part after the decimal to binary
-divisor = 10**len(back)
-back_bin = convert_fraction_to_binary(float(back)/divisor,p)
+    # Step 4:  add the strings together and print the result
+    bin_result = sign + front_bin + "." + back_bin
+    print(bin_result)
 
-# Step 4:  add the strings together and print the result
-result = sign + front_bin + "." + back_bin
-print (result)
-
-# Step 5:  Determine the exponent and mantissa
-if (front_bin == "0"):
-    exponent = 0
-    keep_going = True
-    while keep_going:
-        #print (back_bin[-exponent],exponent)
-        if (back_bin[-exponent] == "1"):
-            keep_going = False
-            exponent=exponent + 1
+    # Step 5:  Determine the exponent and mantissa
+    if front_bin == "0":
+        exponent = 0
+        keep_going = True
+        while keep_going:
+            # print (back_bin[-exponent],exponent)
+            if back_bin[-exponent] == "1":
+                keep_going = False
+                exponent = exponent + 1
+            exponent = exponent - 1
         exponent = exponent - 1
-    exponent = exponent - 1
 
-    #print(exponent)
-    back_bin = back_bin[-exponent:]
-    mantissa_truncated = back_bin
-else:
-    exponent = len(front_bin)-1
-    mantissa = front_bin[1:] + back_bin
-    mantissa_truncated = mantissa[0:23]
+        # print(exponent)
+        back_bin = back_bin[-exponent:]
+        mantissa_truncated = back_bin
+    else:
+        exponent = len(front_bin)-1
+        mantissa = front_bin[1:] + back_bin
+        mantissa_truncated = mantissa[0:23]
 
-true_result = sign + "1." + mantissa_truncated + " x 2^(" + str(exponent) + ")"
-print (true_result)
+    true_result = sign + "1." + mantissa_truncated + " x 2^(" + str(exponent) + ")"
+    print(true_result)
 
-# Step 6:  Convert to 32-bit floating point representation
-if (int(front) < 0):
-    bit1 = "1"
-else:
-    bit1 = "0"
+    # Step 6:  Convert to 32-bit floating point representation
+    if int(front) < 0:
+        bit1 = "1"
+    else:
+        bit1 = "0"
 
-exp = int(exponent)+127
-exp_binary_rep = convert_int_to_binary(exp)
-if (len(exp_binary_rep)<8):
-    exp_binary_rep = "0" + exp_binary_rep
+    exp = int(exponent)+127
+    exp_binary_rep = convert_int_to_binary(exp)
+    if len(exp_binary_rep) < 8:
+        exp_binary_rep = "0" + exp_binary_rep
 
-if (len(mantissa_truncated)<23):
-    mantissa_truncated = mantissa_truncated + (23-len(mantissa_truncated))*"0"
+    if len(mantissa_truncated) < 23:
+        mantissa_truncated = mantissa_truncated + (23-len(mantissa_truncated))*"0"
 
-if (len(mantissa_truncated)>23):
-    mantissa_truncated = mantissa_truncated[0:23]
+    if len(mantissa_truncated) > 23:
+        mantissa_truncated = mantissa_truncated[0:23]
 
-print(bit1 + "|" + exp_binary_rep + "|" + mantissa_truncated)
+    res = bit1 + "|" + exp_binary_rep + "|" + mantissa_truncated
+    return res
 
+
+if __name__ == "__main__":
+
+    fp_num = input("Enter your floating point value : \n")
+    result = get_fp_binary_representation(fp_num)
+    print(result)
--- Zylabs/Chapter2/2.18/main.py
+++ Zylabs/Chapter2/2.18/main.py
@@ -3,4 +3,3 @@ dimes = int(input())
 quarters = int(input())
 dollars = (quarters*25 + dimes*10 + nickels*5) / 100.0
 print(f'Amount: ${dollars:.2f}')
-
