f0e9c048b56165dd1a33a34d8793e4bb5f531a31
Edward Brash <brash@jlab.org>
2023-05-11 12:43:31 -0400

    Update Solitaire plotting

--- Week1_Examples/solitaire.py
+++ Week1_Examples/solitaire.py
@@ -1,7 +1,4 @@
 import random
-import math
-import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 # Initial parameters
@@ -51,30 +48,5 @@ for i in range(numSims):
 
 # Number of bins in the histograms
 h_bins = 200
-
-# Create a 2 x 2 figure
-fig, axs = plt.subplots(2, 2)
-# Add some padding around each subplot, so that title does not overlap with axis labels
-fig.tight_layout(pad=2.0)
-
-# Draw the histograms, with appropriate titles
-axs[0][0].hist(gamesRequired, bins=h_bins)
-axs[0][0].title.set_text('Total Games Required')
-axs[0][1].hist(gamesWon, bins=h_bins)
-axs[0][1].title.set_text('Total Games Won')
-axs[1][0].hist(finalPercentage, bins=h_bins)
-axs[1][0].title.set_text('Overall Win Percentage')
-axs[1][1].hist(latentPercentage, bins=h_bins)
-axs[1][1].title.set_text('Latent Win Percentage')
-
-# Here is a simple (but wrong!) model of what the latentPercentage distribution
-# should look like
-std = 1.0/math.sqrt(chatgpt_prediction)/2.5
-mu = projectedWinPercentage
-
-x_min, x_max = plt.xlim()
-x = np.linspace(x_min, x_max, h_bins)
-p = numSims/h_bins/2.5*norm.pdf(x, mu, std)
-axs[1][1].plot(x, p, 'k', linewidth=2)
-
+plt.hist(gamesRequired, bins=h_bins)
 plt.show()
--- Week1_Examples/solitaire_multiplots.py
+++ Week1_Examples/solitaire_multiplots.py
@@ -1,7 +1,5 @@
 import random
-import math
 import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 numSims = 10000
@@ -11,7 +9,7 @@ projectedWinPercentage = 0.82
 startGames = 2200
 startWins = 1410
 desiredPercentage = 0.65
-#chatgpt_prediction = 211
+# chatgpt_prediction = 211
 chatgpt_prediction = 118
 
 gamesRequired = []
@@ -38,28 +36,69 @@ for i in range(numSims):
     finalPercentage.append(currentWins/currentGames)
     latentPercentage.append((currentWins-startWins)/(currentGames-startGames))
 
-hbins = 200
-
 fig, axs = plt.subplots(2, 2)
 fig.tight_layout(pad=2.0)
-axs[0][0].hist(gamesRequired, bins=hbins)
-axs[0][0].title.set_text('Total Games Required')
-axs[0][1].hist(gamesWon, bins=hbins)
-axs[0][1].title.set_text('Total Games Won')
-axs[1][0].hist(finalPercentage, bins=hbins)
-axs[1][0].title.set_text('Overall Win Percentage')
 
-axs[1][1].hist(latentPercentage, bins=hbins)
-axs[1][1].title.set_text('Latent Win Percentage')
+axs[0][0].plot(gamesRequired, gamesWon, '.')
+axs[0][0].title.set_text('Games Won vs. Games Required')
 
-# Here is a simple (but wrong!) model of what the latentPercentage distribution
-# should look like
-std = 1.0/math.sqrt(chatgpt_prediction)/2.5
-mu = projectedWinPercentage
+axs[0][1].plot(gamesRequired, gamesWon, '.')
+axs[0][1].title.set_text('First Plot Expanded')
+x_low = 2297
+x_high = 2307
+y_low = 1493
+y_high = 1500
+axs[0][1].set_xlim([x_low, x_high])
+axs[0][1].set_ylim([y_low, y_high])
+axs[0][1].set_xticks(np.arange(x_low, x_high, 1))
+axs[0][1].set_yticks(np.arange(y_low, y_high, 1))
+axs[0][1].tick_params(axis='x', rotation=90)
+axs[0][1].xaxis.grid(color='gray', linestyle='dashed')
+axs[0][1].yaxis.grid(color='gray', linestyle='dashed')
 
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, hbins)
-p = numSims/hbins/2.5*norm.pdf(x, mu, std)
-axs[1][1].plot(x, p, 'k', linewidth=2)
+
+xpath1 = [2298, 2299, 2300]
+ypath1 = [1493, 1494, 1495]
+axs[1][0].plot(xpath1, ypath1, 'r.-')
+xpath2 = [2300, 2301, 2302, 2303]
+ypath2 = [1494, 1495, 1496, 1497]
+axs[1][0].plot(xpath2, ypath2, 'g.-')
+xpath3 = [2303, 2304, 2305, 2306]
+ypath3 = [1496, 1497, 1498, 1499]
+axs[1][0].plot(xpath3, ypath3, 'm.-')
+axs[1][0].plot(gamesRequired, gamesWon, 'b.')
+
+axs[1][0].title.set_text('Paths to Victory')
+axs[1][0].set_xlim([x_low, x_high])
+axs[1][0].set_ylim([y_low, y_high])
+axs[1][0].set_xticks(np.arange(x_low, x_high, 1))
+axs[1][0].set_yticks(np.arange(y_low, y_high, 1))
+axs[1][0].tick_params(axis='x', rotation=90)
+axs[1][0].xaxis.grid(color='gray', linestyle='dashed')
+axs[1][0].yaxis.grid(color='gray', linestyle='dashed')
+
+xpath1 = [2298, 2299, 2300]
+ypath1 = [1493, 1494, 1495]
+axs[1][1].plot(xpath1, ypath1, 'r.-')
+xpath2 = [2300, 2301, 2302, 2303]
+ypath2 = [1494, 1495, 1496, 1497]
+axs[1][1].plot(xpath2, ypath2, 'g.-')
+xpath3 = [2303, 2304, 2305, 2306]
+ypath3 = [1496, 1497, 1498, 1499]
+axs[1][1].plot(xpath3, ypath3, 'm.-')
+axs[1][1].plot(gamesRequired, gamesWon, 'b.')
+
+x4 = [2297+i for i in range(10)]
+y4 = [0.65*x4[i] for i in range(10)]
+axs[1][1].plot(x4,y4,'c--')
+
+axs[1][1].title.set_text('Paths to Victory')
+axs[1][1].set_xlim([x_low, x_high])
+axs[1][1].set_ylim([y_low, y_high])
+axs[1][1].set_xticks(np.arange(x_low, x_high, 1))
+axs[1][1].set_yticks(np.arange(y_low, y_high, 1))
+axs[1][1].tick_params(axis='x', rotation=90)
+axs[1][1].xaxis.grid(color='gray', linestyle='dashed')
+axs[1][1].yaxis.grid(color='gray', linestyle='dashed')
 
 plt.show()
