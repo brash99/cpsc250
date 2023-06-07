df99b44b38dd468db7857b086f85c4452fb0ae06
brash99 <brash@jlab.org>
2023-06-07 14:55:09 -0400

    Updates after class 2

--- Week1_Examples/convert_int_to_binary.py
+++ Week1_Examples/convert_int_to_binary.py
@@ -1,11 +1,16 @@
+# Using built-in function
 num = 11
+b = bin(num)
+print(num, " = ", b, " in binary.")
+
 num_original = num
 res = ""
 
 while num > 0:
     digit = num % 2
-    #print(num, digit)
-    res = str(digit)+res
     num = int(num/2)
+    # print(num, digit)
+    res = str(digit)+res
+
 
 print(num_original, " = ", res, " in binary.")
--- Week1_Examples/convert_int_to_hex.py
+++ Week1_Examples/convert_int_to_hex.py
@@ -4,12 +4,12 @@ base = 16
 num_original = num
 res = ""
 
-digit_strings = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+digit_strings = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
 while num > 0:
     digit = num % base
-    #print(num, digit)
+    # print(num, digit)
     res = digit_strings[digit]+res
     num = int(num/base)
 
-print(num_original, " = ", res, " in base ",base)
+print(num_original, " = ", res, " in base ", base)
