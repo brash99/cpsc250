faf90e79c35c67bc1f16f8765182f345fbee3ede
brash99 <brash@jlab.org>
2023-06-10 10:27:37 -0400

    Week 2 updates

---
+++ Week2_Examples/derived_classes.py
@@ -0,0 +1,65 @@
+class Item:
+    def __init__(self):
+        self.name = ''
+        self.quantity = 0
+
+    def set_name(self, nm):
+        self.name = nm
+
+    def get_name(self):
+        return self.name
+
+    def set_quantity(self, qnty):
+        self.quantity = qnty
+
+    def get_quantity(self):
+        return self.quantity
+
+    def display(self):
+        print(self.name, self.quantity)
+
+
+class Produce(Item):  # Derived from Item
+    def __init__(self):
+        Item.__init__(self)  # Call base class constructor
+        self.expiration = ''
+
+    def set_expiration(self, expir):
+        self.expiration = expir
+
+    def get_expiration(self):
+        return self.expiration
+
+
+class Banana(Produce):  # Derived from Produce
+    def __init__(self):
+        Produce.__init__(self)
+        self.color = ''
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+
+item1 = Item()
+item1.set_name('Cereal')
+item1.set_quantity(9)
+item1.display()
+
+item2 = Produce()
+item2.set_name('Apples')
+item2.set_quantity(20)
+item2.set_expiration('8/5/2023')
+item2.display()
+print(f'  Expires: {item2.get_expiration()}')
+
+item3 = Banana()
+item3.set_name('Chiquita')
+item3.set_quantity(50)
+item3.set_expiration('7/4/2023')
+item3.set_color('yellow')
+item3.display()
+print(f'  Expires: {item3.get_expiration()}')
+print(f'  Color: {item3.get_color()}')
---
+++ Week2_Examples/derived_classes2.py
@@ -0,0 +1,72 @@
+class TransportMode:
+    def __init__(self, name, speed=75):
+        self.name = name
+        self.speed = speed
+
+    def info(self):
+        print(f'{self.name} can go {self.speed} mph.')
+
+
+class MotorVehicle(TransportMode):
+    def __init__(self, name, speed=80, mpg=40):
+        TransportMode.__init__(self, name, speed)
+        self.mpg = mpg
+        self.fuel_gal = 0
+
+    def add_fuel(self, amount):
+        self.fuel_gal += amount
+
+    def drive(self, dist):
+        required_fuel = dist / self.mpg
+        if self.fuel_gal < required_fuel:
+            print('Not enough gas.')
+        else:
+            self.fuel_gal -= required_fuel
+            print(f'{self.fuel_gal:f} gallons remaining.')
+
+
+class MotorCycle(MotorVehicle):
+    def __init__(self, name, speed=55, mpg=25):
+        MotorVehicle.__init__(self, name, speed, mpg)
+
+    def wheelie(self):
+        print('That is too dangerous.')
+
+
+# Define two motorcycles, specifying all values
+scooter = MotorCycle('Vespa', 55, 40)
+dirtbike = MotorCycle('KX450F', 80, 25)
+
+# Define some vehicles, specifying only the name ... note which
+# default values are used :)
+harley = MotorCycle('Harley Davidson')
+generic = MotorVehicle('PeopleCarrier')
+generic2 = TransportMode('MovingThing')
+
+scooter.info()
+dirtbike.info()
+harley.info()
+generic.info()
+generic2.info()
+
+
+choice = input('Select scooter (s) or dirtbike (d): ')
+bike = scooter if (choice == 's') else dirtbike
+
+menu = '\nSelect add fuel(f), go(g), wheelie(w), quit(q): '
+command = input(menu)
+while command != 'q':
+    if command == 'f':
+        fuel = int(input('Enter amount: '))
+        bike.add_fuel(fuel)
+    elif command == 'g':
+        distance = int(input('Enter distance: '))
+        bike.drive(distance)
+    elif command == 'w':
+        bike.wheelie()
+    elif command == 'q':
+        break
+    else:
+        print('Invalid command.')
+
+    command = input(menu)
