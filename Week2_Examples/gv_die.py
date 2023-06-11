5a3ef35d79a97cc2b74c46b4ecc68efa8c322c5a
brash99 <brash@jlab.org>
2023-06-11 09:27:01 -0400

    Week 2 updates

---
+++ Week2_Examples/gv_die.py
@@ -0,0 +1,19 @@
+import random
+
+class GVDie:
+
+    # Set initial die value
+    def __init__(self):
+        self.value = None
+
+    # Roll the die to get 1 - 6
+    def roll(self):
+      self.value = random.randint(1, 6)
+
+    # Return current die value
+    def get_value(self):
+      return self.value
+
+    # Allows dice to be compared if necessary
+    def compare_to(self, d):
+        return self.value == d.value
---
+++ Week2_Examples/zylab23.py
@@ -0,0 +1,12 @@
+import random
+import gv_die
+
+# Read random seed to support testing (do not alter) and starting credits
+seed = int(input())
+# Set the seed for random
+random.seed(int(seed))
+
+# Initial credits
+credits = int(input())
+
+# Type your code here.
