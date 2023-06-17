04230ad96dd67b81d98e93ed5c390bd3fb68e620
brash99 <brash@jlab.org>
2023-06-17 08:50:38 -0400

    Week 3 updates

---
+++ LectureNotes/Week3_Lectures/fibonacci.py
@@ -0,0 +1,25 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+
+if __name__ == "__main__":
+
+    nth_term = 36
+
+    if nth_term % 10 == 0:
+        ending = "th"
+    elif nth_term % 10 == 1:
+        ending = "st"
+    elif nth_term % 10 == 2:
+        ending = "nd"
+    elif nth_term % 10 == 3:
+        ending = "rd"
+    else:
+        ending = "th"
+
+    print(f"The {nth_term}{ending} term of the Fibonacci sequence is {fibonacci(nth_term)}")
---
+++ LectureNotes/Week3_Lectures/root_finding.py
@@ -0,0 +1,40 @@
+def f(x):
+    return -4.0*x + 3.0
+
+
+def find_root(a, b):
+    epsilon = 1.0E-8
+    fa = f(a)
+    fb = f(b)
+    # print(a,b,fa,fb)
+    if abs(fa-fb) < epsilon:
+        print("Type 1: Found root at x = ", a)
+    else:
+        c = (a+b)/2.0
+        fc = f(c)
+        if fc == 0.0:
+            print("Type 2: Found root at x = ", c)
+        else:
+            if fa*fc < 0.0:
+                find_root(a, c)
+            else:
+                find_root(c, b)
+
+
+if __name__ == "__main__":
+
+    a = 0.0
+    b = 5.0
+    fa = f(a)
+    fb = f(b)
+
+    if fa*fb > 0:
+        print(a,b,fa,fb)
+        print("No root on this interval!!")
+    elif fa*fb == 0:
+        if fa == 0:
+            print("Type 3a: Found root at x = ", a)
+        else:
+            print("Type 3b: Found root at x = ", b)
+    else:
+        find_root(a, b)
