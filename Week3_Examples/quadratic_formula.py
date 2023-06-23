d2172f2f1a4d20f1ccab67f0bb05122131159333
brash99 <brash@jlab.org>
2023-06-23 10:50:34 -0400

    Week 3 updates

--- Week3_Examples/quadratic_formula.py
+++ Week3_Examples/quadratic_formula.py
@@ -3,10 +3,15 @@ import math
 if __name__ == '__main__':
 
     a = 1.0
-    b = 5.0
-    c = 0.0
+    b = -3.0
+    c = 1.0
 
     root1 = (-b + math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
     root2 = (-b - math.sqrt(b ** 2 - 4.0 * a * c)) / (2.0 * a)
 
     print(root1, root2)
+
+    for x in (root1,root2):
+        f = x**4 + 1/x**4
+        print(f)
+
