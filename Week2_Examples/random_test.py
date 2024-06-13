2eed424521ac88d849d2f7564d7c4f59c34f868c
brash99 <brash@jlab.org>
2024-06-13 08:59:57 -0400

    Week 4 updates

---
+++ Week2_Examples/random_test.py
@@ -0,0 +1,19 @@
+import random
+
+seed = 42
+random.seed(seed)
+
+number_of_randoms = 10
+
+for i in range(number_of_randoms):
+    r_num = random.randint(1, 100)
+    print(f"Random number {i+1}: {r_num}")
+
+#for i in range(number_of_randoms):
+#    r_num = random.random()
+#    print(f"Random number {i+1}: {r_num}")
+
+#my_choices = ['rock', 'paper', 'scissors']
+#for i in range(number_of_randoms):
+#    r_choice = random.choice(my_choices)
+#    print(f"Random choice {i+1}: {r_choice}")
