7de6e1ec5e95b210cd3000ff90bc9a47cbcd3bae
brash99 <brash@jlab.org>
2024-06-17 22:04:56 -0400

    Updates

--- Week3_Examples/PlantInformation.py
+++ Week3_Examples/PlantInformation.py
@@ -1,30 +1,43 @@
 from PlantFlower import Plant, Flower
 
-
 def print_list(garden):
     for i in range(len(garden)):
         print(f'Plant {i + 1} Information:')
         garden[i].print_info()
         print()
 
-
 if __name__ == "__main__":
-    my_garden = []
-    user_string = input()
 
-    while user_string != '-1':
-        tokens = user_string.split()
-        plant_type = tokens[0]
-        plant_name = tokens[1]
-        plant_cost = tokens[2]
+    my_garden = [] # Create an empty list to store the plants and flowers
+
+    user_string = input() # Get the first line of input
+
+    while user_string != '-1': # Continue until the user enters -1
+        # Process the line of input
+        line_elements = user_string.split() # Split the line into list of strings
+
+        # Extract the plant type, plant name, and plant cost (first three elements)
+        # from the list of strings
+        # These will exist for both plants AND flowers
+        plant_type = line_elements[0]
+        plant_name = line_elements[1]
+        plant_cost = line_elements[2]
+
+        # Create the appropriate object, and add it to the list
+
         if plant_type == 'plant':
+            # If the plant type is a plant, create a plant object (we have all the info)
             my_plant = Plant(plant_name, plant_cost)
             my_garden.append(my_plant)
         elif plant_type == 'flower':
-            is_annual = tokens[3]
-            color_of_flowers = tokens[4]
+            # If the plant type is a flower, extract the remaining two elements
+            # from the list of strings
+            is_annual = line_elements[3]
+            color_of_flowers = line_elements[4]
+            # Create a flower object (we now have all the info)
             my_flower = Flower(plant_name, plant_cost, is_annual, color_of_flowers)
             my_garden.append(my_flower)
+
         user_string = input()
 
     print_list(my_garden)
---
+++ Week3_Examples/VehicleInformation_Template.py
@@ -0,0 +1,24 @@
+class Vehicle:
+    def __init__(self, brand="Vehicle", model="Generic"):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Move!")
+
+
+# TODO: complete the Car, Plane, and Boat classes
+class Car:
+    pass
+
+
+class Plane:
+    pass
+
+
+class Boat:
+    pass
+
+
+if __name__ == '__main__':
+# TODO:  write main program
