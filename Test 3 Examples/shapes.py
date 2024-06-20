f917f2531b49764b99a1567f50afd3e8252d855b
brash99 <brash@jlab.org>
2024-06-19 22:24:28 -0400

    Updates

--- Test 3 Examples/shapes.py
+++ Test 3 Examples/shapes.py
@@ -88,3 +88,20 @@ if __name__ == '__main__':
 
     for shape in shapes:
         print(shape)
+    print()
+
+    rectangle2 = Rectangle(4, 1, 1)
+    rectangle2.set_length(2)
+    rectangle2.set_width(4)
+    print(rectangle2)  # 4 sides, area=8, perimeter=12
+    print(rectangle2.get_length())  # 2
+    print(rectangle2.get_width())  # 4
+
+    circle2 = Circle(1, 1)
+    circle2.set_radius(2)
+    print(circle2)  # 1 side, area=12.57, perimeter=12.57
+    print(circle2.get_radius())  # 2
+    print()
+
+    print(rectangle2 == Rectangle(4, 4, 2)) # False
+    print(circle2 == Circle(1, 2))  # True
