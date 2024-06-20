2d522d0795c8d06f8bb1057a1cfabe38c21eec98
brash99 <brash@jlab.org>
2024-06-19 22:13:48 -0400

    Updates

---
+++ Test 3 Examples/shapes.py
@@ -0,0 +1,90 @@
+class Shape:
+
+    def __init__(self, sides):
+        self.number_of_sides = sides
+
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+    def set_number_of_sides(self, sides):
+        self.number_of_sides = sides
+
+    def get_number_of_sides(self):
+        return self.number_of_sides
+
+
+class Rectangle(Shape):
+
+    def __init__(self, sides, length, width):
+        super().__init__(sides)
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_length(self, length):
+        self.length = length
+
+    def get_width(self):
+        return self.width
+
+    def get_length(self):
+        return self.length
+
+    def __eq__(self, other):
+        return self.length==other.length and self.width==other.width
+
+    def __str__(self):
+        return f'A rectangle has {self.number_of_sides} sides | Area = {self.calculate_area()} | Perimeter = {self.calculate_perimeter()}'
+
+class Circle(Shape):
+
+    def __init__(self, sides, radius):
+        super().__init__(sides)
+        self.radius = radius
+
+    def calculate_area(self):
+        return 3.14159265 * self.radius**2
+
+    def calculate_perimeter(self):
+        return 2 * 3.14159265 * self.radius
+
+    def set_radius(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+    def __eq__(self, other):
+        return self.radius==other.radius
+
+    def __str__(self):
+        return f'A circle has {self.number_of_sides} side | Area = {self.calculate_area():.2f} | Perimeter = {self.calculate_perimeter():.2f}'
+
+if __name__ == '__main__':
+    rectangle = Rectangle(4, 4.1, 2.3)
+    print(rectangle)  # 4 sides, area=9.43, perimeter=12.8
+
+    circle = Circle(1, 3.5)
+    print(circle)  # 1 side, area=38.48, perimeter=21.99
+
+    print()
+
+    shapes = [
+        Circle(1, 3.2),  # 1 side, area=32.17, perimeter=20.11
+        Rectangle(4, 2, 2.6),  # 4 sides, area=5.20, perimeter=9.20
+        Circle(1, 8.3),  # 1 side, area=216.42, perimeter=52.15
+    ]
+
+    for shape in shapes:
+        print(shape)
