8e979707f7ebcf628516a314d72386c83bcf8753
Edward Brash <brash@jlab.org>
2024-06-22 17:56:56 -0400

    Week 4 updates

--- Week3_Examples/Circle.py
+++ Week3_Examples/Circle.py
@@ -1,12 +1,55 @@
 import math
 
-
 class Circle:
+
+    # When a circle object is created, we have two options:
+    # 1. We can create a circle object with a radius value
+    # 2. We can create a circle object without a radius value
+    # If we create a circle object without a radius value, the default value of the radius is 0.0
+
     def __init__(self, radius=0.0):
         self.radius = radius
 
+    # Define a method to calculate the area of the circle
+    # This function will NOT take any parameters when we
+    # call it. It will use the radius value that is stored
+    # in the object to calculate the area of the circle
     def compute_area(self):
         return math.pi*self.radius**2
 
+    # Define a method to calculate the circumference of the circle
+    # This function will NOT take any parameters when we call it.
+    # It will use the radius value that is stored in the object to
+    # calculate the circumference of the circle
     def compute_circumference(self):
         return 2.0*math.pi*self.radius
+
+    # Define a method to get the radius of the circle
+    # This function will NOT take any parameters when we call it.
+    # It will return the radius value that is stored in the object
+    def get_radius(self):
+        return self.radius
+
+    # Define a method to set the radius of the circle
+    # This function will take a single parameter, radius, when we call it.
+    # It will set the radius value that is stored in the object to the value
+    # that we pass in as a parameter
+    def set_radius(self, radius):
+        self.radius = radius
+
+    # Define a str method to return a string representation of the object
+    # This function MUST NOT take any parameters when we call it.
+    # It will return a string that includes the radius value that is stored in the object
+    # This function is called when we use the print function to print the object
+    def __str__(self):
+        return "Circle with radius: " + str(self.radius)
+
+    # Define an eq method to compare two objects
+    # This function will take two parameters when we call it:
+    # self: The first Circle object
+    # other: The second Circle object
+    # It will compare the radius value of the two objects
+    # It will return True if the radius value of the two objects is the same
+    # It will return False if the radius value of the two objects is different
+    def __eq__(self, other):
+        return self.radius == other.radius
--- Week3_Examples/circle_unittest.py
+++ Week3_Examples/circle_unittest.py
@@ -2,7 +2,6 @@ import unittest
 from Circle import Circle
 import math
 
-
 class TestCircle(unittest.TestCase):
     def test_area(self):
         c = Circle(0.0)
@@ -10,10 +9,43 @@ class TestCircle(unittest.TestCase):
         c = Circle(5.0)
         self.assertEqual(c.compute_area(), math.pi*5.0**2)
 
-    def test_smart(self):
-        c = Circle(5)
+    def test_positive_circumference(self):
+        c = Circle(5.0)
         self.assertGreaterEqual(c.compute_circumference(), 0.0)
+        #c = Circle(-5.0)
+        #self.assertGreaterEqual(c.compute_circumference(), 0.0)
 
+    def test_get_radius(self):
+        c = Circle(5.0)
+        self.assertEqual(c.get_radius(), 5.0)
+
+    def test_set_radius(self):
+        c = Circle(5.0)
+        c.set_radius(10.0)
+        self.assertEqual(c.get_radius(), 10.0)
+        c.set_radius(0.0)
+        self.assertEqual(c.get_radius(), 0.0)
+        c.set_radius(-5.0)
+        self.assertEqual(c.get_radius(), -5.0)
+
+    def test_str(self):
+        c = Circle(5.0)
+        self.assertEqual(str(c), "Circle with radius: 5.0")
+        c = Circle(0.0)
+        self.assertEqual(str(c), "Circle with radius: 0.0")
+        c = Circle(-5.0)
+        self.assertEqual(str(c), "Circle with radius: -5.0")
+
+    def test_eq(self):
+        c1 = Circle(5.0)
+        c2 = Circle(5.0)
+        self.assertTrue(c1 == c2)
+        c2 = Circle(10.0)
+        self.assertFalse(c1 == c2)
+        c2 = Circle(0.0)
+        self.assertFalse(c1 == c2)
+        c2 = Circle(-5.0)
+        self.assertFalse(c1 == c2)
 
 if __name__ == "__main__":
     unittest.main()
